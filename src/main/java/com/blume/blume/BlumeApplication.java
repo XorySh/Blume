f630da42df9804d93899f7a26db9e2c60b50d260
XorySh <melchordiazreyes@gmail.com>
2023-12-09 04:31:23 +0100

    first commit

---
+++ src/main/java/com/blume/blume/BlumeApplication.java
@@ -0,0 +1,13 @@
+package com.blume.blume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlumeApplication.class, args);
+	}
+
+}
